b6e02ed76940216afca39e02fd842bb0f1c3b0e3
yuanjiang <cqwkkj@gmail.com>
2023-05-17 23:28:36 +0800

    feat:初始化react项目

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  server: {
+    port: 4000, // 设置服务启动端口号
+    open: true, // 设置服务启动时是否自动打开浏览器
+    cors: true // 允许跨域
+
+    // 设置代理，根据我们项目实际情况配置
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://xxx.xxx.xxx.xxx:8000',
+    //     changeOrigin: true,
+    //     secure: false,
+    //     rewrite: (path) => path.replace('/api/', '/')
+    //   }
+    // }
+  },
+  build: {
+    // 设置最终构建的浏览器兼容目标
+    target: 'es2015',
+    // 构建后是否生成 source map 文件
+    sourcemap: false,
+    //  chunk 大小警告的限制（以 kbs 为单位）
+    chunkSizeWarningLimit: 2000,
+    // 启用/禁用 gzip 压缩大小报告
+    reportCompressedSize: false
+  }
+})
