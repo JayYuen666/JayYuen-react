7c80cf49e3e6ea47e5d61c759b8b5ff11e5afaf1
JayYuen <53552772+JayYuen666@users.noreply.github.com>
2023-05-31 20:45:41 +0800

    Update tools.ts
    添加加减乘除运算

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -155,6 +155,71 @@ function hasOwnProperty<T>(obj: T, v: PropertyKey) {
   }
   return Object.prototype.hasOwnProperty.call(obj, v)
 }
+/**
+  * 加减乘除运算
+  */
+const accAdd = (arg1, arg2) => {
+    let r1, r2, m;
+    try {
+        r1 = arg1.toString().split(".")[1].length;
+    } catch (e) {
+        r1 = 0;
+    };
+    try {
+        r2 = arg2.toString().split(".")[1].length;
+    } catch (e) {
+        r2 = 0;
+    };
+    m = Math.pow(10, Math.max(r1, r2));
+    return (arg1 * m + arg2 * m) / m;
+};
+const accMul = (arg1, arg2) => {
+    let m = 0;
+    let s1 = arg1.toString();
+    let s2 = arg2.toString();
+    try {
+        m += s1.split(".")[1].length;
+    } catch (e) { };
+    try {
+        m += s2.split(".")[1].length;
+    } catch (e) { };
+    return Number(s1.replace(".", "")) * Number(s2.replace(".", "")) / Math.pow(10, m);
+};
+const accDiv = (arg1, arg2) => {
+    let s1 = arg1.toString();
+    let s2 = arg2.toString();
+    let t1 = 0;
+    let t2 = 0;
+    let r1;
+    let r2;
+    try {
+        t1 = s1.split(".")[1].length;
+    } catch (e) { };
+    try {
+        t2 = s2.split(".")[1].length;
+    } catch (e) { };
+    r1 = Number(s1.replace(".", ""));
+    r2 = Number(s2.replace(".", ""));
+    return (r1 / r2) * Math.pow(10, t2 - t1);
+};
+const calcFn = {
+    add() {
+        let arg = Array.from(arguments);
+        return arg.reduce((total, num) => accAdd(total, num));
+    },
+    sub() {
+        let arg = Array.from(arguments);
+        return arg.reduce((total, num) => accAdd(total, num * -1));
+    },
+    mul() {
+        let arg = Array.from(arguments);
+        return arg.reduce((total, num) => accMul(total, num));
+    },
+    div() {
+        let arg = Array.from(arguments);
+        return arg.reduce((total, num) => accDiv(total, num));
+    },
+};
 
 export {
   roundToFixed,
