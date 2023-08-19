ae0d0393673c670092b4eefd7a39b0b553123bcb
yuanjiang <cqwkkj@gmail.com>
2023-08-19 18:00:19 +0800

    ✨ feat: 引入antd

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ConfigProvider } from 'antd';
+import dayjs from 'dayjs';
+import 'dayjs/locale/zh-cn';
+import zhCN from 'antd/locale/zh_CN';
 import '@unocss/reset/normalize.css'
 import App from './App.tsx'
-
+dayjs.locale('zh-cn');
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <ConfigProvider locale={zhCN}>
     <App />
+    </ConfigProvider>
   </React.StrictMode>
 )
